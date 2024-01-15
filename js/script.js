f3f8735bd10ac6158d8a2ff94924aa4c2ec518b9
unknown <fadhlanaulia14@gmail.com>
2024-01-16 04:21:30 +0700

    Commit ke 2, dari yutub part2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-// Toggle Class Actice
+// Toggle Class Actice Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu diklik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Toggle Class Actice Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+
+searchButton.onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
 };
 
 // klik diluar sidebar untuk menghilangkan nav
@@ -13,3 +25,55 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// klik diluar sidebar untuk menghilangkan search form
+document.addEventListener("click", function (e) {
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
+
+// Toggle Active untuk Shopping Card
+const shoppingButton = document.querySelector("#shopping-cart-button");
+const removeItem = document.querySelector(".remove-item");
+const shoppingCard = document.querySelector(".shopping-card");
+
+shoppingButton.onclick = (e) => {
+  shoppingCard.classList.toggle("active");
+  e.preventDefault();
+};
+// klik diluar sidebar untuk menghilangkan shopping card
+document.addEventListener("click", function (e) {
+  if (
+    !shoppingButton.contains(e.target) &&
+    !removeItem.contains(e.target) &&
+    !shoppingCard.contains(e.target)
+  ) {
+    shoppingCard.classList.remove("active");
+  }
+});
+
+// Toggle melihat detail
+const detailModalButtons = document.querySelectorAll(".detail-button");
+const detailModal = document.querySelector("#item-detail-modal");
+const closeModal = document.querySelector(".close-icon");
+
+detailModalButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    detailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik Close untuk menghilangkan Modal
+closeModal.onclick = (e) => {
+  detailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar sidebar untuk menghilangkan Modal
+window.onclick = (e) => {
+  if (e.target === detailModal) {
+    detailModal.style.display = "none";
+  }
+};
